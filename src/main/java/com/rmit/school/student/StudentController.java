69d58b649f753b57b860f0f5a3dca57d67858a6d
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-06-27 22:44:20 +0700

    Update CRUD Operations

--- src/main/java/com/rmit/school/student/StudentController.java
+++ src/main/java/com/rmit/school/student/StudentController.java
@@ -6,10 +6,14 @@ import java.util.List;
 import com.rmit.school.student.StudentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,5 +35,19 @@ public class StudentController {
     @PostMapping
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId) {
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+        @PathVariable("studentId") Long studentId,
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) String email) 
+        {
+        studentService.updateStudent(studentId, name, email);
     }
 }
--- src/main/java/com/rmit/school/student/StudentRepository.java
+++ src/main/java/com/rmit/school/student/StudentRepository.java
@@ -3,11 +3,14 @@ package com.rmit.school.student;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    @Query("SELECT s FROM Student s WHERE s.email = ?1 ")
     Optional<Student> findStudentByEmail(String email);
 }
